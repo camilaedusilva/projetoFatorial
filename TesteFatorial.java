9193292e848e6f273557a784189f60797608fb97
Camila Edu Silva <camilae.silva@gmail.com>
2023-05-15 12:47:52 -0300

    Calculo de faotrial de 10 e renomeação

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -4,7 +4,7 @@ class TesteFatorial {
 	//comentario inserido teste
 	pulic static void main(string[] args){
 		Fatorial fat = new Fatorial();
-		int resultado = fat.calcularFatorial(0);
-		system.out.println(resultado);
+		int r = fat.calcularFatorial(10);
+		system.out.println(r);
 	}
 }
